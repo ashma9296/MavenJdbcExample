6a48d925f49c6139a403283671a2411abbeca7eb
ashmask <akhadka@urgent.ly>
2020-05-24 13:07:58 -0400

    modified select query

--- src/main/java/com/xmn/project/SelectQuery.java
+++ src/main/java/com/xmn/project/SelectQuery.java
@@ -18,27 +18,24 @@ public class SelectQuery {
 		 * stmt.executeUpdate(insertSql);
 		 */
 
-		String selectSql = "select address from user where Fname = 'ashma' ";
+		String selectSql = "select * from user where Fname = 'ashma' ";
 		ResultSet rs = stmt.executeQuery(selectSql);
 
 		while (rs.next()) {
 			// one row
-			
-//			  int Userid = rs.getInt("UserId");
-//			   String Fname = rs.getString("Fname");
-//			  String Lname = rs.getString("Lname");
-			 
-			String address = rs.getString("address");
-			// int salary = rs.getInt("salary");
-			// System.out.println(" User Details : UserId = " + Userid + " , name = " +
-			// Fname + " , Lname = " + Lname + " , address = " + address +" , salary = " +
-			// salary);
-			System.out.println("address : " + address);
-		}
-		
-		
 
-		String updateSql = " update user set address = '9335 lee highway' where userid = " +  1;
+			int Userid = rs.getInt("UserId");
+			String Fname = rs.getString("Fname");
+			String Lname = rs.getString("Lname");
+
+			String address = rs.getString("address");
+			int salary = rs.getInt("salary");
+			System.out.println(" User Details : UserId = " + Userid + " , name = " + Fname + " , Lname = " + Lname
+					+ " , address = " + address + " , salary = " + salary);
+			// System.out.println("address : " + address);
+		}
+
+		String updateSql = " update user set address = '9335 lee highway' where userid = " + 1;
 		stmt.executeUpdate(updateSql);
 
 	}
